0c9a596b69be16537c45bb11ec7ac5bd69807ec3
민성홍 <sunghong32@naver.com>
2021-04-27 12:02:14 +0900

    앱 아이콘 추가, 앱 이름 추가, 앱 다듬기

--- CH01 기본기능 다루기 /MyMemory/MyMemory/MemoReadVC.swift
+++ CH01 기본기능 다루기 /MyMemory/MyMemory/MemoReadVC.swift
@@ -9,21 +9,26 @@ import UIKit
 
 class MemoReadVC: UIViewController {
 
+    var param : MemoData?
+    @IBOutlet weak var subjects: UILabel!
+    @IBOutlet weak var contents: UILabel!
+    @IBOutlet weak var img: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // 1. 제목과 내용, 이미지를 출력
+        self.subjects.text = param?.title
+        self.contents.text = param?.contents
+        self.img.image = param?.image
+        
+        // 2. 날짜 포맷 변환
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd일 HH:mm분에 작성됨"
+        let dateString = formatter.string(from: (param?.regdate)!)
+        
+        // 3. 내비게이션 타이틀에 날짜를 표시
+        self.navigationItem.title = dateString
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
