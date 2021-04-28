ac852d8cc7e39403f4019893c8e35035f081e648
민성홍 <sunghong32@naver.com>
2021-04-28 22:58:38 +0900

    커스텀 탭 바 구현

--- CH03 커스터마이징(2)/Chapter03-TabBar/Chapter03-TabBar/SceneDelegate.swift
+++ CH03 커스터마이징(2)/Chapter03-TabBar/Chapter03-TabBar/SceneDelegate.swift
@@ -10,13 +10,48 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        // 1. 루트 뷰 컨트롤러를 UITabBarController로 캐스팅한다.
+        if let tbC = self.window?.rootViewController as? UITabBarController {
+            
+            // 2. 탭 바로부터 탭 바 아이템 배열을 가져온다.
+            if let tbItems = tbC.tabBar.items {
+                
+                 //3. 탭 바 아이템에 커스텀 이미지를 등록한다.
+//                tbItems[0].image = UIImage(named: "calendar")
+//                tbItems[1].image = UIImage(named: "file-tree")
+//                tbItems[2].image = UIImage(named: "photo")
+                
+                tbItems[0].image = UIImage(named: "designbump")?.withRenderingMode(.alwaysOriginal)
+                tbItems[1].image = UIImage(named: "rss")?.withRenderingMode(.alwaysOriginal)
+                tbItems[2].image = UIImage(named: "facebook")?.withRenderingMode(.alwaysOriginal)
+                
+                // 탭 바 아이템 전체를 순회하면서 selectedImage 속성에 이미지를 설정한다.
+                for tbItem in tbItems {
+                    let image = UIImage(named: "checkmark")?.withRenderingMode(.alwaysOriginal)
+                    tbItem.selectedImage = image
+                    
+                    // 전체 아이템의 폰트 크기를 설정한다.
+                    tbItem.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15)], for: .normal) 
+                }
+
+                // 4. 탭 바 아이템에 타이틀을 설정한다.
+                tbItems[0].title = "calendar"
+                tbItems[1].title = "file"
+                tbItems[2].title = "photo"
+            }
+            
+            // 5. 활성화된 탭 바 아이템의 이미지 색상을 변경한다.
+            tbC.tabBar.tintColor = UIColor.white
+            
+            // 6. 탭 바에 배경 이미지를 설정한다.
+            tbC.tabBar.backgroundImage = UIImage(named: "menubar-bg-mini")
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
