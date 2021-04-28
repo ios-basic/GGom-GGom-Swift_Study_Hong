002337b39321a5734a96221eb9af48d7ad2fb8ce
민성홍 <sunghong32@naver.com>
2021-04-28 20:52:18 +0900

    커스텀 탭바 구현

---
+++ CH03 커스터마이징(2)/Chapter03-TabBar/Chapter03-TabBar/AppDelegate.swift
@@ -0,0 +1,58 @@
+//
+//  AppDelegate.swift
+//  Chapter03-TabBar
+//
+//  Created by 민성홍 on 2021/04/28.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // 1. 루트 뷰 컨트롤러를 UITabBarController로 캐스팅한다.
+        if let tbC = self.window?.rootViewController as? UITabBarController {
+            
+            // 2. 탭 바로부터 탭 바 아이템 배열을 가져온다.
+            if let tbItems = tbC.tabBar.items {
+                
+                // 3. 탭 바 아이템에 커스텀 이미지를 등록한다.
+                tbItems[0].image = UIImage(named: "calendar")
+                tbItems[1].image = UIImage(named: "file-tree")
+                tbItems[2].image = UIImage(named: "photo")
+
+                // 4. 탭 바 아이템에 타이틀을 설정한다.
+                tbItems[0].title = "calendar"
+                tbItems[1].title = "file"
+                tbItems[2].title = "photo"
+            }
+            
+            // 5. 활성화된 탭 바 아이템의 이미지 색상을 변경한다.
+            tbC.tabBar.tintColor = UIColor.white
+            
+            // 6. 탭 바에 배경 이미지를 설정한다.
+            tbC.tabBar.backgroundImage = UIImage(named: "menubar-bg-mini")
+        }
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
