c676ceb353a71be82fc2ce5718f61207437d594f
민성홍 <sunghong32@naver.com>
2021-05-11 22:46:12 +0900

    UserDefaults를 사용한 데이터 저장

---
+++ CH05 데이터저장(1) - 프로퍼티 리스트/Chapter05-UserDefaults/Chapter05-UserDefaults/ListViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ListViewController.swift
+//  Chapter05-UserDefaults
+//
+//  Created by 민성홍 on 2021/05/11.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var gender: UISegmentedControl!
+    @IBOutlet weak var married: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        return 0
+//    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            // 입력이 가능한 알림창을 띄워 이름을 수정할 수 있도록 한다.
+            let alert = UIAlertController(title: nil, message: "이름을 입력사요", preferredStyle: .alert)
+            
+            // 입력 필드 추가
+            alert.addTextField() {
+                $0.text = self.name.text // name 레이블의 텍스트를 입력폼에 기본값으로 넣어준다.
+            }
+            
+            // 버튼 및 액션 추가
+            alert.addAction(UIAlertAction(title: "OK", style: .default){(_) in
+                // 사용자가 OK버튼을 누르면 입력 필드에 입력된 값을 저장한다.
+                let value = alert.textFields?[0].text
+                
+                let plist = UserDefaults.standard
+                plist.setValue(value, forKey: "name")
+                plist.synchronize()
+                
+                self.name.text = value
+            })
+            
+            // 알림창을 띄운다.
+            self.present(alert, animated: false, completion: nil)
+        }
+    }
+    
+    @IBAction func changeGender(_ sender: UISegmentedControl) {
+        let value = sender.selectedSegmentIndex // 0이면 남자, 1이면 여자
+        
+        let plist = UserDefaults.standard // 기본 저장소 객체를 가져온다.
+        plist.set(value,forKey: "gender") // "gender"라는 키로 값을 저장한다.
+        plist.synchronize() // 동기화 처리
+    }
+    
+    @IBAction func changeMarried(_ sender: UISwitch) {
+        let value = sender.isOn // true면 기혼, false면 미혼
+        
+        let plist = UserDefaults.standard
+        plist.set(value, forKey: "married")
+        plist.synchronize()
+    }
+    
+}
